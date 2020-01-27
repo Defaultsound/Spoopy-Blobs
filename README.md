a3ee7dc1f575a780a18018e77683acf053266a91
Alex <lxdownham@gmail.com>
2020-01-27 15:53:28 +0000

    Updated Project and included a timer, also updated Read me

--- README.md
+++ README.md
@@ -1,6 +1,39 @@
-# SpoopyBlobs
-Simple game made in Godot
+# Spoopy Blobs
 
-Made as a simple learning excerise. 
+![screenshot](./screenshots/screenshot.png)
 
-All assets either taken from Opengamedevart or Itch.io
+---
+
+## About
+
+Simple game made in Godot 3.1
+
+This game was made as a simple learning excerise. All code can be found within this repository.
+
+Overall this should merely be used as reference and learning material. Most of the code and structure is probably not best practice, however thought those who are learning Godot may want to see it.
+
+Play it here: https://defaultsound.itch.io/spoopy-blobs
+
+---
+## How to play
+
+WASD - Move
+
+Collect all the peas
+
+Use the keys
+
+Get back to the beginning to win
+
+Avoid the Spoopy Blobs
+
+---
+
+## Resources
+
+All assets either taken from https://opengameart.org/ or https://itch.io/ (Sorry I couldn't credit all, forgotten where some of the sounds/fonts came from)
+
+Main Sprite Sheet: https://fisherg.itch.io/micro-asset-pack
+
+Music: https://opengameart.org/content/happy-accident
+
--- scripts/UI.gd
+++ scripts/UI.gd
@@ -2,7 +2,11 @@ extends Control
 
 onready var pea_count = $CenterContainer/HBoxContainer/HBoxContainer/Pea_Count
 onready var key_count = $CenterContainer/HBoxContainer/HBoxContainer2/Key_Count
+onready var time_text =$HBoxContainer3/Time_Count
 onready var win_text = $"CenterContainer2/YOU WIN"
+onready var game_over = false
+onready var ui_timer = $Timer
+onready var milliseconds_elapsed = 0
 
 func set_pea_count(val):
 	pea_count.set_text(str(val))
@@ -13,4 +17,20 @@ func set_key_count(val):
 	pass
 	
 func show_win():
-	win_text.set("visible",true)	
+	game_over = true
+	win_text.set("visible",true)
+	
+func timer():
+	if game_over != true:
+		if ui_timer.time_left == 0:
+			if milliseconds_elapsed < 215999:
+				milliseconds_elapsed += 1
+				var milliseconds  = milliseconds_elapsed % 60
+				var seconds = milliseconds_elapsed % 3600 / 60
+				var minutes = milliseconds_elapsed / 3600 % 60
+				var str_elapsed = "%02d . %02d . %02d" % [minutes, seconds, milliseconds ]
+				time_text.set_text(str_elapsed)
+				ui_timer.start()
+	
+func _process(delta):
+	timer()
